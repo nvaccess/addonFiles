257e9281efc0b71da893e2ffaddab435c742fed9
Mesar Hameed <mhameed@src.gnome.org>
2013-02-09 06:21:47 +0100

    get.php: add ocr addon.

--- get.php
+++ get.php
@@ -3,6 +3,7 @@ $addons = array(
     "dx" => = "dropbox-3.1.nvda-addon",
     "ew" => "extendedWinamp-1.1.nvda-addon",
     "it" => "instantTranslate-2.2beta1.nvda-addon",
+    "ocr" => "http://files.nvaccess.org/nvda-addons/ocr_0.20120529.01.nvda-addon",
     "st" => "systrayList-1.4.nvda-addon"
 );
 
