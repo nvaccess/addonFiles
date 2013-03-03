c47f9c2d191c9bee06c76bb3d9e6b38b97f670ff
Mesar Hameed <mhameed@src.gnome.org>
2013-03-03 12:49:01 +0100

    added unicodeBrailleInput addon.

--- get.php
+++ get.php
@@ -4,7 +4,8 @@ $addons = array(
     "ew" => "extendedWinamp-1.1.nvda-addon",
     "it" => "instantTranslate-2.2beta1.nvda-addon",
     "ocr" => "http://files.nvaccess.org/nvda-addons/ocr_0.20120529.01.nvda-addon",
-    "st" => "systrayList-1.4.nvda-addon"
+    "st" => "systrayList-1.4.nvda-addon",
+    "ubi" => "unicodeBrailleInput-1.0.nvda-addon"
 );
 
 If (isset($_GET['file'])) {
