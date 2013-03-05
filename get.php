6739c5d67bc782b9d61ad47ee47a0f75cde673c6
beqa <beqaprogger@gmail.com>
2013-03-05 20:43:35 +0400

    made final decision and changed version to 2.2-dev

--- get.php
+++ get.php
@@ -4,7 +4,7 @@ $addons = array(
     "ew" => "extendedWinamp-1.1.nvda-addon",
     "it" => "instantTranslate-2.2beta1.nvda-addon",
     "ocr" => "http://files.nvaccess.org/nvda-addons/ocr_0.20120529.01.nvda-addon",
-    "rm" => "resourceMonitor-2.1.nvda-addon",
+    "rm" => "resourceMonitor-2.2-dev.nvda-addon",
     "st" => "systrayList-1.4.nvda-addon",
     "ubi" => "unicodeBrailleInput-1.0.nvda-addon"
 );
