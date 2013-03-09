185d8eca7b024728cb5710b896d39476ba56e062
Mesar Hameed <mhameed@src.gnome.org>
2013-03-09 13:11:55 +0100

    added word addon.

--- get.php
+++ get.php
@@ -6,7 +6,8 @@ $addons = array(
     "ocr" => "http://files.nvaccess.org/nvda-addons/ocr_0.20120529.01.nvda-addon",
     "rm" => "resourceMonitor-2.2-dev.nvda-addon",
     "st" => "systrayList-1.4.nvda-addon",
-    "ubi" => "unicodeBrailleInput-1.01.nvda-addon"
+    "ubi" => "unicodeBrailleInput-1.01.nvda-addon",
+    "wrd" => "word-0.2-dev.nvda-addon"
 );
 
 If (isset($_GET['file'])) {
