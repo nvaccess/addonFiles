03e41f0946a44d290f6b1c011bd08a819480c2f5
Mesar Hameed <mhameed@src.gnome.org>
2013-07-07 14:32:01 +0200

    virtualRevision 1.3-dev.

--- get.php
+++ get.php
@@ -9,7 +9,7 @@ $addons = array(
     "rm" => "resourceMonitor-2.3.nvda-addon",
     "st" => "systrayList-2.0-dev.nvda-addon",
     "ubi" => "unicodeBrailleInput-1.01.nvda-addon",
-	"VR" => "virtualRevision-1.2.1dev.nvda-addon",
+	"VR" => "virtualRevision-1.3-dev.nvda-addon",
     "wrd" => "word-0.2-dev.nvda-addon"
 );
 
