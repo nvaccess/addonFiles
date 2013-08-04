16bb7c4e6c7a81e594e028ff3e842895364c9bc4
Mesar Hameed <mhameed@src.gnome.org>
2013-08-04 08:56:00 +0200

    make focus highlight available for download.

--- get.php
+++ get.php
@@ -3,6 +3,7 @@ $addons = array(
     "cua" => "controlUsageAssistant-1.0.nvda-addon",
     "dx" => "dropbox-3.1.nvda-addon",
     "ew" => "extendedWinamp-1.1.nvda-addon",
+    "fh" => "focusHighlight-0.0.5.nvda-addon",
     "it" => "instantTranslate-2.2beta2.nvda-addon",
     "nb" => "NoBeepsSpeechMode-1.2.nvda-addon",
     "ocr" => "http://files.nvaccess.org/nvda-addons/ocr_0.20120529.01.nvda-addon",
