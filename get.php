e85a1c0a04d7146b74b623d398d0d9dedd1f4766
Noelia Ruiz Martínez <nrm1977@gmail.com>
2013-10-27 21:30:27 +0100

    Updated URL for eMule-1.0 stable add-on.

--- get.php
+++ get.php
@@ -2,7 +2,7 @@
 $addons = array(
     "cua" => "controlUsageAssistant-2.1.nvda-addon",
     "dx" => "dropbox-3.1.nvda-addon",
-    "em" => "eMule-1.0-dev.nvda-addon",
+    "em" => "eMule-1.0.nvda-addon",
 	"emo-dev" => "emoticons-1.0-dev.nvda-addon",
     "ew" => "extendedWinamp-1.1.nvda-addon",
     "fh-dev" => "focusHighlight-1.0-dev.nvda-addon",
