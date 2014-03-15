7645e420fa6f671f25294db460dfb56fbb19c8a0
Alberto Buffolino <a.buffolino@gmail.com>
2014-03-15 18:46:58 +0100

    Regularized spacing using only tab

--- get.php
+++ get.php
@@ -1,60 +1,60 @@
 <?php
 $addons = array(
-    "bc" => "bitChe-1.4.nvda-addon",
-    "bc-dev" => "bitChe-1.5-dev.nvda-addon",
-    "cua" => "controlUsageAssistant-2.1.nvda-addon",
-    "dx" => "dropbox-3.1.nvda-addon",
-    "dx-dev" => "dropbox-4.0-dev.nvda-addon",
-    "em" => "eMule-1.1.nvda-addon",
+	"bc" => "bitChe-1.4.nvda-addon",
+	"bc-dev" => "bitChe-1.5-dev.nvda-addon",
+	"cua" => "controlUsageAssistant-2.1.nvda-addon",
+	"dx" => "dropbox-3.1.nvda-addon",
+	"dx-dev" => "dropbox-4.0-dev.nvda-addon",
+	"em" => "eMule-1.1.nvda-addon",
 	"em-dev" => "eMule-1.2-dev.nvda-addon",
 	"emo" => "emoticons-1.0.nvda-addon",
-    "emo-dev" => "emoticons-1.1-dev.nvda-addon",
-    "ets" => "enhancedTouchGestures-0.1.nvda-addon",
-    "ew" => "extendedWinamp-1.1.nvda-addon",
-    "fh" => "focusHighlight-1.0.nvda-addon",
-    "fh-dev" => "focusHighlight-1.1-dev.nvda-addon",
-    "gl" => "https://bitbucket.org/nvdaaddonteam/todo/raw/master/guideLines.txt",
+	"emo-dev" => "emoticons-1.1-dev.nvda-addon",
+	"ets" => "enhancedTouchGestures-0.1.nvda-addon",
+	"ew" => "extendedWinamp-1.1.nvda-addon",
+	"fh" => "focusHighlight-1.0.nvda-addon",
+	"fh-dev" => "focusHighlight-1.1-dev.nvda-addon",
+	"gl" => "https://bitbucket.org/nvdaaddonteam/todo/raw/master/guideLines.txt",
 	"gsr" => "googleSpeechRecognition-1.0.nvda-addon",
-    "gsr-dev" => "googleSpeechRecognition-1.1-dev.nvda-addon",
-    "gwv" => "goldwave-1.2.nvda-addon",
-    "gwv-dev" => "goldwave-2.0-dev.nvda-addon",
-    "it" => "instantTranslate-2.2beta2.nvda-addon",
-    "it-dev" => "instantTranslate-3.0-dev.nvda-addon",
-    "nb" => "NoBeepsSpeechMode-1.4.nvda-addon",
-    "ocr" => "http://files.nvaccess.org/nvda-addons/ocr_0.20120529.01.nvda-addon",
-    "pm" => "placeMarkers-3.0.nvda-addon",
-    "pm-dev" => "placeMarkers-3.1-dev.nvda-addon",
+	"gsr-dev" => "googleSpeechRecognition-1.1-dev.nvda-addon",
+	"gwv" => "goldwave-1.2.nvda-addon",
+	"gwv-dev" => "goldwave-2.0-dev.nvda-addon",
+	"it" => "instantTranslate-2.2beta2.nvda-addon",
+	"it-dev" => "instantTranslate-3.0-dev.nvda-addon",
+	"nb" => "NoBeepsSpeechMode-1.4.nvda-addon",
+	"ocr" => "http://files.nvaccess.org/nvda-addons/ocr_0.20120529.01.nvda-addon",
+	"pm" => "placeMarkers-3.0.nvda-addon",
+	"pm-dev" => "placeMarkers-3.1-dev.nvda-addon",
 	"rf" => "readFeeds-1.0.nvda-addon",
-    "rf-dev" => "readFeeds-1.1-dev.nvda-addon",
-    "rm" => "resourceMonitor-3.1.nvda-addon",
-    "rm-dev" => "resourceMonitor-3.1.nvda-addon",
-    "rs-dev" => "rapidSettings-2.0dev.nvda-addon",
-    "st" => "systrayList-2.0-dev.nvda-addon",
-    "spl" => "StationPlaylist-1.2.nvda-addon",
-    "spl-dev" => "StationPlaylist-2.0-dev.nvda-addon",
-    "tv" => "teamViewer-1.0.nvda-addon",
-    "tv-dev" => "teamViewer-1.1-dev.nvda-addon",
-    "ubi" => "unicodeBrailleInput-1.01.nvda-addon",
-    "VR" => "virtualRevision-1.3-dev.nvda-addon",
-    "wrd" => "word-0.2-dev.nvda-addon"
+	"rf-dev" => "readFeeds-1.1-dev.nvda-addon",
+	"rm" => "resourceMonitor-3.1.nvda-addon",
+	"rm-dev" => "resourceMonitor-3.1.nvda-addon",
+	"rs-dev" => "rapidSettings-2.0dev.nvda-addon",
+	"st" => "systrayList-2.0-dev.nvda-addon",
+	"spl" => "StationPlaylist-1.2.nvda-addon",
+	"spl-dev" => "StationPlaylist-2.0-dev.nvda-addon",
+	"tv" => "teamViewer-1.0.nvda-addon",
+	"tv-dev" => "teamViewer-1.1-dev.nvda-addon",
+	"ubi" => "unicodeBrailleInput-1.01.nvda-addon",
+	"VR" => "virtualRevision-1.3-dev.nvda-addon",
+	"wrd" => "word-0.2-dev.nvda-addon"
 );
 
 If (isset($_GET['file'])) {
-    $file = $_GET['file'];
-    $fullname = $addons[$file];
+	$file = $_GET['file'];
+	$fullname = $addons[$file];
 
-    if ($fullname != "") {
-        Header('Location:'.$fullname);
-    } else {
-        print "<h1>File not found</h1>";
-        print "<p>The file you requested could not be found on this server.</p>";
-        print "<p>Please check that the link that brought you here is correct and try again.</p>";
-        print "<p>Thanks</p>";
-    }
+	if ($fullname != "") {
+		Header('Location:'.$fullname);
+	} else {
+		print "<h1>File not found</h1>";
+		print "<p>The file you requested could not be found on this server.</p>";
+		print "<p>Please check that the link that brought you here is correct and try again.</p>";
+		print "<p>Thanks</p>";
+	}
 } else {
-    print "<h1>Error:</h1>";
-    print "<p>Please check that the link that brought you here is correct and try again.</p>";
-    print "<p>If you continue to see this message report this error to the nvda addon list.</p>";
-    print "<p>Thanks</p>";
+	print "<h1>Error:</h1>";
+	print "<p>Please check that the link that brought you here is correct and try again.</p>";
+	print "<p>If you continue to see this message report this error to the nvda addon list.</p>";
+	print "<p>Thanks</p>";
 }
 ?>
