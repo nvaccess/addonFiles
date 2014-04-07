368313fe620aae1bab19c65946649d2c097deec1
Joseph Lee <joseph.lee22590@gmail.com>
2014-04-06 23:07:30 -0700

    Quick Books 2014 add-on from NV Access is ready for public review.

--- get.php
+++ get.php
@@ -24,6 +24,7 @@ $addons = array(
 	"ocr" => "http://files.nvaccess.org/nvda-addons/ocr_0.20120529.01.nvda-addon",
 	"pm" => "placeMarkers-3.0.nvda-addon",
 	"pm-dev" => "placeMarkers-3.1-dev.nvda-addon",
+	"qb-dev" => "http://files.nvaccess.org/nvda-addons/quickBooks2014-2014.4.7.nvda-addon",
 	"rf" => "readFeeds-1.0.nvda-addon",
 	"rf-dev" => "readFeeds-1.1-dev.nvda-addon",
 	"rm" => "resourceMonitor-3.1.nvda-addon",
