150c55ed66cee32437a47ee0c3a2a3b6f79599d3
Joseph Lee <joseph.lee22590@gmail.com>
2014-06-07 21:05:40 -0700

    BitChe 1.5.

--- get.php
+++ get.php
@@ -1,6 +1,6 @@
 <?php
 $addons = array(
-	"bc" => "bitChe-1.4.nvda-addon",
+	"bc" => "bitChe-1.5.nvda-addon",
 	"bc-dev" => "bitChe-1.5-dev.nvda-addon",
 	"cua" => "controlUsageAssistant-2.1.nvda-addon",
 	"dx" => "dropbox-3.1.nvda-addon",
