0dac69765e21383133597f2995dbaf4ccdc83894
Noelia Ruiz Martínez <nrm1977@gmail.com>
2014-12-03 19:10:47 +0100

    BitChe: bump version to 2.1: added translations updates.

--- get.php
+++ get.php
@@ -1,7 +1,7 @@
 <?php
 $addons = array(
-	"bc" => "bitChe-2.0.nvda-addon",
-	"bc-dev" => "bitChe-2.0.nvda-addon",
+	"bc" => "bitChe-2.1.nvda-addon",
+	"bc-dev" => "bitChe-2.1.nvda-addon",
 	"cua" => "controlUsageAssistant-2.1.nvda-addon",
 	"dx" => "dropbox-3.1.nvda-addon",
 	"dx-dev" => "dropbox-4.0-dev.nvda-addon",
