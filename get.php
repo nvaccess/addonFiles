d5c417793cc4e3826536cda1516bff8da0ef3896
Joseph Lee <joseph.lee22590@gmail.com>
2016-06-07 03:23:43 -0700

    WinTenApps 20160607-dev

--- get.php
+++ get.php
@@ -52,7 +52,7 @@ $addons = array(
 	"vlc" => "vlc-1.2.nvda-addon",
 	"vent" => "Ventrilo-1.0-dev.nvda-addon",
 	"w10" => "wintenApps-16.05.nvda-addon",
-	"w10-dev" => "wintenApps-20160603-dev.nvda-addon",
+	"w10-dev" => "wintenApps-20160607-dev.nvda-addon",
 	"wrd" => "word-0.2-dev.nvda-addon"
 );
 
