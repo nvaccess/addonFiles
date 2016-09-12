a2ebb844834449e8edc64d7fec4b6d423a57d821
Joseph Lee <joseph.lee22590@gmail.com>
2016-09-12 07:58:56 -0700

    WinTenApps 20160912-dev, remove Enhanced Touch Gestures dev as it's no longer needed

--- get.php
+++ get.php
@@ -62,7 +62,7 @@ $addons = array(
 	"vlc" => "vlc-1.2.nvda-addon",
 	"vent" => "Ventrilo-1.0-dev.nvda-addon",
 	"w10" => "https://github.com/josephsl/wintenApps/releases/download/16.09.1/wintenApps-16.09.1A.nvda-addon",
-	"w10-dev" => "wintenApps-20160910-dev.nvda-addon",
+	"w10-dev" => "wintenApps-20160912-dev.nvda-addon",
 	"wrd" => "word-0.2-dev.nvda-addon"
 );
 
