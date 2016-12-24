6f53f4588a4a4e1715087c0c436514e7ea867cc9
Joseph Lee <joseph.lee22590@gmail.com>
2016-12-24 10:17:37 -0800

    Classic Selection 1.0 stable (on behalf of Tyler Spivey)

--- get.php
+++ get.php
@@ -4,6 +4,7 @@ $addons = array(
 	"ath-dev" => "AudioThemes3D-5.1-beta.nvda-addon",
 	"bc" => "bitChe-2.6.nvda-addon",
 	"bc-dev" => "bitChe-2.6.nvda-addon",
+	"clsel" => "https://github.com/tspivey/classicSelection/releases/download/v1.0/classicSelection-1.0.nvda-addon",
 	"ccd" => "https://github.com/nvdaaddons/clipContentsDesigner/releases/download/v4.1/clipContentsDesigner-4.1.nvda-addon",
 	"ccd-dev" => "https://github.com/nvdaes/clipContentsDesigner/releases/download/5.0-dev/clipContentsDesigner-5.0-dev.nvda-addon",
 	"cr-dev" => "columnsReview-3.0-dev.nvda-addon",
