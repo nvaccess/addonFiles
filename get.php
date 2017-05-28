0211dbf48a76e5fb14d6e1461694577c2ccb9202
Joseph Lee <joseph.lee22590@gmail.com>
2017-05-28 10:51:15 -0700

    Object Location Tones May 28th snapshot

--- get.php
+++ get.php
@@ -41,6 +41,7 @@ $addons = array(
 	"mush" => "mushClient-1.3.nvda-addon",
 	"nb" => "noBeepsSpeechMode-2.3.nvda-addon",
 	"lambda-dev" => "https://github.com/nvdaaddons/lambda/releases/download/dev/lambda-dev.nvda-addon",
+	"objLoc" => "objLocTones-20170528-dev.nvda-addon",
 	"objPad" => "https://github.com/josephsl/objPad/releases/download/16.12/objPad-16.12.nvda-addon",
 	"objPad-dev" => "https://github.com/josephsl/objPad/releases/download/16.12/objPad-16.12.nvda-addon",
 	"ocr" => "https://www.nvaccess.org/files/nvda-addons/ocr_0.20120529.01.nvda-addon",
