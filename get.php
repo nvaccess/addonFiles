5e8378c1b92a36bf97a40ad8c3a5930be8f65ee9
Joseph Lee <joseph.lee22590@gmail.com>
2017-06-28 11:39:45 -0700

    WinTenApps 17.07A emergency snapshot

--- get.php
+++ get.php
@@ -77,7 +77,7 @@ $addons = array(
 	"VR" => "virtualRevision-2.2.nvda-addon",
 	"vlc" => "vlc-1.3.nvda-addon",
 	"vent" => "Ventrilo-1.0-dev.nvda-addon",
-	"w10" => "https://github.com/josephsl/wintenApps/releases/download/17.07/wintenApps-17.07.nvda-addon",
+	"w10" => "https://github.com/josephsl/wintenApps/releases/download/17.07/wintenApps-17.07A.nvda-addon",
 	"w10-dev" => "http://www.josephsl.net/files/nvdaaddons/getupdate.php?file=w10-dev",
 	"wetp" => "http://www.nvda.it/system/files/plugin/weather_plus4.4.nvda-addon?download=1",
 	"wrd" => "word-0.2-dev.nvda-addon"
