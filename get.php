45f183370960301cdf82222cf161aeb92a96ed06
Joseph Lee <joseph.lee22590@gmail.com>
2018-04-03 12:21:43 -0700

    Braille Extender dev link

--- get.php
+++ get.php
@@ -6,6 +6,7 @@ $addons = array(
 	"bc-dev" => "bitChe-2.6.nvda-addon",
 	"bgt" => "https://github.com/sykesman/NVDA-BGT/releases/download/1.0-dev/bgt_lullaby-1.0-dev.nvda-addon",
 	"brlext" => "https://andreabc.net/projects/NVDA_addons/BrailleExtender/latest",
+	"brlext-dev" => "https://andreabc.net/projects/NVDA_addons/BrailleExtender/latest?channel=dev",
 	"clsel" => "https://github.com/tspivey/classicSelection/releases/download/v1.0/classicSelection-1.0.nvda-addon",
 	"ccd" => "https://github.com/nvdaes/clipContentsDesigner/releases/download/6.4/clipContentsDesigner-6.4.nvda-addon",
 	"ccd-dev" => "https://github.com/nvdaes/clipContentsDesigner/releases/download/6.4/clipContentsDesigner-6.4.nvda-addon",
