bf2c03d531d057b0c7560b080f3900583768c06e
Joseph Lee <joseph.lee22590@gmail.com>
2018-06-10 16:33:18 -0700

    SPL add-on 18.06.1

--- get.php
+++ get.php
@@ -66,7 +66,7 @@ $addons = array(
 	"rsy" => "https://github.com/nvdaes/reportSymbols/releases/download/3.5/reportSymbols-3.5.nvda-addon",
 	"rsy-dev" => "https://github.com/nvdaes/reportSymbols/releases/download/4.0-dev/reportSymbols-4.0-dev.nvda-addon",
 	"sentencenav" => "https://github.com/mltony/nvda-sentence-nav/releases/download/v1.1/SentenceNav-1.1.nvda-addon",
-	"spl" => "https://github.com/josephsl/stationPlaylist/releases/download/18.06/stationPlaylist-18.06.nvda-addon",
+	"spl" => "https://github.com/josephsl/stationPlaylist/releases/download/18.06.1/stationPlaylist-18.06.1.nvda-addon",
 	"spl-dev" => "http://www.josephsl.net/files/nvdaaddons/getupdate.php?file=spl-dev",
 	"sps" => "https://github.com/jscholes/nvda-speech-history/releases/download/v2017.05.13/speechHistory.nvda-addon",
 	"st" => "systrayList-2.3.nvda-addon",
