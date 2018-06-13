abc91058a1021d001a1f6545653625c46752f1fc
Joseph Lee <joseph.lee22590@gmail.com>
2018-06-13 01:39:18 -0700

    WintenApps 18.06A channel switcher

--- get.php
+++ get.php
@@ -84,7 +84,7 @@ $addons = array(
 	"VR" => "virtualRevision-2.2.nvda-addon",
 	"vlc" => "vlc-1.3.nvda-addon",
 	"vent" => "Ventrilo-1.0-dev.nvda-addon",
-	"w10" => "https://github.com/josephsl/wintenApps/releases/download/18.06/wintenApps-18.06.nvda-addon",
+	"w10" => "https://github.com/josephsl/wintenApps/releases/download/18.06/wintenApps-18.06A.nvda-addon",
 	"w10-dev" => "http://www.josephsl.net/files/nvdaaddons/getupdate.php?file=w10-dev",
 	"wetp" => "http://www.nvda.it/files/plugin/weather_plus4.7.nvda-addon",
 	"wrd" => "word-0.2-dev.nvda-addon"
