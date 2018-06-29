55a2538e5f64765684942999c312abe2135ab70d
Noelia Ruiz Martínez <nrm1977@gmail.com>
2018-06-29 13:12:54 +0200

    Add vlc-dev

--- get.php
+++ get.php
@@ -85,6 +85,7 @@ $addons = array(
 	"VR" => "virtualRevision-2.2.nvda-addon",
 	"vlc" => "vlc-1.3.nvda-addon",
 	"vlc-18" => "https://github.com/javidominguez/VLC/releases/download/2.2/VLC-2.2.nvda-addon",
+	"vlc-dev" => "https://github.com/javidominguez/VLC/releases/download/2.5dev/VLC-2.5dev.nvda-addon",
 	"vent" => "Ventrilo-1.0-dev.nvda-addon",
 	"w10" => "https://github.com/josephsl/wintenApps/releases/download/18.07/wintenApps-18.07.nvda-addon",
 	"w10-dev" => "http://www.josephsl.net/files/nvdaaddons/getupdate.php?file=w10-dev",
