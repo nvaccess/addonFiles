c28cc0583a3dff9c41ea1f6763ae341bbd892b51
Joseph Lee <joseph.lee22590@gmail.com>
2018-11-30 08:00:03 -0800

    StationPlaylist Studio 18.12

--- get.php
+++ get.php
@@ -82,7 +82,7 @@ $addons = array(
 	"rsy-o" => "https://github.com/nvdaes/reportSymbols/releases/download/3.6/reportSymbols-3.6.nvda-addon",
 	"sentencenav" => "https://github.com/mltony/nvda-sentence-nav/releases/download/v2.0/SentenceNav-2.0.nvda-addon",
 	"spie" => "https://www.nvaccess.org/files/nvda-addons/speechPlayerInEspeak-0.4.nvda-addon",
-	"spl" => "https://github.com/josephsl/stationPlaylist/releases/download/18.11/stationPlaylist-18.11.2A.nvda-addon",
+	"spl" => "https://github.com/josephsl/stationPlaylist/releases/download/18.12/stationPlaylist-18.12.nvda-addon",
 	"spl-lts18" => "https://github.com/josephsl/stationPlaylist/releases/download/18.11/stationPlaylist-18.09.5A-lts.nvda-addon",
 	"spl-dev" => "https://www.josephsl.net/files/nvdaaddons/getupdate.php?file=spl-dev",
 	"sps" => "https://github.com/jscholes/nvda-speech-history/releases/download/v2017.05.13/speechHistory.nvda-addon",
