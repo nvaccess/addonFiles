7f9d888a09908fe4e5acccfb004f4459b479b19d
abdel792 <abdelkrim.bensaid@gmail.com>
2019-01-03 12:07:46 +0100

    Clock-19.01-dev

--- get.php
+++ get.php
@@ -10,7 +10,7 @@ $addons = array(
 	"brlext" => "https://andreabc.net/projects/NVDA_addons/BrailleExtender/latest",
 	"brlext-dev" => "https://andreabc.net/projects/NVDA_addons/BrailleExtender/latest?channel=dev",
 	"btaudio" => "https://github.com/mltony/nvda-bluetooth-audio/releases/download/v1.0/bluetoothaudio-1.0.nvda-addon",
-	"cac" => "http://cyber25.free.fr/nvda-addons/clock-18.12.nvda-addon",
+	"cac" => "https://github.com/hkatic/clock/releases/download/v19.01-dev/clock-19.01-dev.nvda-addon",
 	"cac-dev" => "https://github.com/hkatic/clock/releases/download/v19.01-dev/clock-19.01-dev.nvda-addon",
 	"cae" => "https://github.com/javidominguez/Calibre/releases/download/1.2/calibre-1.2.nvda-addon",
 	"cae-dev" => "https://github.com/javidominguez/Calibre/releases/download/1.2/calibre-1.2.nvda-addon",
