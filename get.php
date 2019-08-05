ae1ce8958924b071ebc2a105d20d8e5cc38f72b6
Joseph Lee <joseph.lee22590@gmail.com>
2019-08-05 16:36:58 -0700

    Access8Math 2.3

--- get.php
+++ get.php
@@ -1,6 +1,6 @@
 <?php
 $addons = array(
-	"access8math" => "https://github.com/tsengwoody/Access8Math/releases/download/v2.1/Access8Math-2.1.nvda-addon",
+	"access8math" => "https://github.com/tsengwoody/Access8Math/releases/download/v2.3/Access8Math-2.3.nvda-addon",
 	"addonshelp" => "https://github.com/ruifontes/addonsHelp/releases/download/2.0/addonsHelp-2.0.nvda-addon",
 	"ath" => "https://github.com/mush42/Audio-Themes-NVDA-Add-on/releases/download/v5.1/AudioThemes3D-5.1.nvda-addon",
 	"ath-dev" => "https://github.com/mush42/Audio-Themes-NVDA-Add-on/releases/download/v5.1/AudioThemes3D-5.1.nvda-addon",
