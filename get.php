abfc078f4b147a0abbf3143e79898b35cf96a5ea
Joseph Lee <joseph.lee22590@gmail.com>
2019-11-27 11:51:41 -0800

    Windows 10 App Essentials 19.11.3

--- get.php
+++ get.php
@@ -127,7 +127,7 @@ $addons = array(
 	"vlc-18" => "https://github.com/javidominguez/VLC/releases/download/2.10/VLC-2.10.nvda-addon",
 	"vlc-dev" => "https://github.com/javidominguez/VLC/releases/download/2.10/VLC-2.10.nvda-addon",
 	"vent" => "Ventrilo-1.0-dev.nvda-addon",
-	"w10" => "https://github.com/josephsl/wintenApps/releases/download/19.11/wintenApps-19.11.2.nvda-addon",
+	"w10" => "https://github.com/josephsl/wintenApps/releases/download/19.11/wintenApps-19.11.3.nvda-addon",
 	"w10-dev" => "https://www.josephsl.net/files/nvdaaddons/getupdate.php?file=w10-dev",
 	"wc" => "https://github.com/ruifontes/wordCount/releases/download/1.4/wordCount-1.4.nvda-addon",
 	"wetp" => "http://www.nvda.it/files/plugin/weather_plus7.2.nvda-addon",
