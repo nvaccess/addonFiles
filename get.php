c6683b95b91d0b84d1497bd24c1c5a24256c7b90
Joseph Lee <joseph.lee22590@gmail.com>
2022-09-23 22:59:43 -0700

    Windows App Essentials 22.10

--- get.php
+++ get.php
@@ -153,7 +153,7 @@ $addons = array(
 	"vlc" => "https://github.com/javidominguez/VLC/releases/download/2.14/VLC-2.14.nvda-addon",
 	"vlc-dev" => "https://github.com/javidominguez/VLC/releases/download/2.13/VLC-2.13.nvda-addon",
 	"vent" => "Ventrilo-1.0-dev.nvda-addon",
-	"w10" => "https://github.com/josephsl/wintenApps/releases/download/22.09/wintenApps-22.09.1.nvda-addon",
+	"w10" => "https://github.com/josephsl/wintenApps/releases/download/22.10/wintenApps-22.10.nvda-addon",
 	"w10-dev" => "https://www.josephsl.net/files/nvdaaddons/getupdate.php?file=w10-dev",
 	"wc" => "https://github.com/ruifontes/wordCount/releases/download/2022.03/wordCount-2022.03.nvda-addon",
 	"wetp" => "https://www.nvda.it/files/plugin/weather_plus8.9.nvda-addon",
